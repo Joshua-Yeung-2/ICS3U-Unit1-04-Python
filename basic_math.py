be1ce1c60bb134540aadddea7f365ff3e98896c3
Ubuntu <ubuntu@ip-172-31-7-96.ec2.internal>
2021-10-21 16:15:54 +0000

    some commit message

---
+++ basic_math.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+
+# Created by Joshua Yeung
+# Created on September 2021
+# This program does basic math
+
+
+def main():
+    # this program does basic math
+
+    print("let the dimensions of rectangle be: ")
+    print("4 cm x 20 cm")
+    print("")
+    print("a = 4 x 20")
+    print("a = {}.".format(4 * 20))
+    print("Area is {} cm².".format(4 * 20))
+    print("")
+    print("p = 2 x (4 + 20)")
+    print("p = 2 x 24")
+    print("p = {}.".format(2 * (4 + 20)))
+    print("Perimeter is {} cm.".format(2 * (4 + 20)))
+
+
+if __name__ == "__main__":
+    main()
